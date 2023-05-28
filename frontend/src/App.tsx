dfaae7fe563fe55953ac7b2ae112a00ea3f9e0cb
Jan Nouza <xnouza@fi.muni.cz>
2023-05-28 11:18:32 +0200

    feat(frontend): Project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className='text-blue-600'>Hello World</div>
+    </Router>
+  );
+}
+
+export default App;
