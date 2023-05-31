ca71d3a9a06a4f508753e4d50440f1f91773d3e8
Jan Nouza <xnouza@fi.muni.cz>
2023-05-31 20:43:47 +0200

    fix(Header): Styling Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,46 +1,44 @@
 export const Header = () => {
-  return (
-    <div className="flex flex-col h-24 w-screen fixed top-0">
-            <header className="flex flex-row w-full border-black border-b-4 max-h-full rounded-md justify-between items-center pt-2 pb-2">
-                <img src="./../../logo-placeholder.png" className="max-h-full h-auto border-r-2 border-black" />
-                <div className="flex flex-row h-1/2 w-1/4 items-center">
-                    <input type="text" placeholder="Hledat" className="border-2 border-black border-r-0 h-full w-full self-center p-1 rounded-md rounded-tr-none rounded-br-none outline-none" />
-                    <a className="h-full w-auto p-1 border-black border-2 border-l-0 rounded-md rounded-tl-none rounded-bl-none cursor-pointer">
-                        <img src="./../../mag_glass.png" className="max-h-full" />
-                    </a>
-                </div>
-                <div className="flex flex-row justify-between w-1/3 h-1/2 items-center">
-                    <a className="inline-flex border-black border-2 rounded-md p-1 w-20 items-center justify-center cursor-pointer">
-                        Hry
-                    </a>
-                    <a className="inline-flex border-black border-2 rounded-md p-1 w-20 items-center justify-center cursor-pointer">
-                        Žánry
-                    </a>
-                    <a className="inline-flex border-black border-2 rounded-md p-1 w-20 items-center justify-center cursor-pointer">
-                        Studia
-                    </a>
-                </div>
+    return (
+        <div className="flex flex-row w-full h-16 fixed top-0 bg-gray-800 text-white justify-between items-center px-4 shadow-md">
+            <img src="./../../logo-placeholder.png" className="h-10 w-auto mr-2" />
+            <div className="flex flex-row items-center border-2 border-gray-600 rounded-full w-1/3">
+                <input type="text" placeholder="Hledat" className="rounded-l-full h-full w-full pl-2 bg-gray-800 outline-none text-white" />
+                <button className="p-2 rounded-r-full bg-gray-700 hover:bg-gray-600 transition-colors duration-300 ease-in-out">
+                    <img src="./../../mag_glass.png" className="h-6 w-6" />
+                </button>
+            </div>
+            <div className="flex flex-row justify-between w-1/3 items-center">
+                <button className="px-3 py-1 rounded-md text-white bg-gray-700 hover:bg-gray-600 transition-colors duration-300 ease-in-out">
+                    Hry
+                </button>
+                <button className="px-3 py-1 rounded-md text-white bg-gray-700 hover:bg-gray-600 transition-colors duration-300 ease-in-out">
+                    Žánry
+                </button>
+                <button className="px-3 py-1 rounded-md text-white bg-gray-700 hover:bg-gray-600 transition-colors duration-300 ease-in-out">
+                    Studia
+                </button>
+            </div>
 
-                {/* Visible to anonymous user */}
-                <div className="flex flex-col h-full w-32 justify-between items-center pl-2 pr-2 border-l-2 border-black">
-                    <a className="inline-flex w-full h-full items-center justify-center border-b-2 border-black cursor-pointer">
-                        Přihlásit
-                    </a>
-                    <a className="inline-flex w-full h-full items-center justify-center cursor-pointer">
-                        Registrovat
-                    </a>
-                </div>
+            {/* Visible to anonymous user */}
+            <div className="flex flex-col h-full justify-between items-center pl-2 pr-2 border-l-2 border-gray-700">
+                <button className="w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out">
+                    Přihlásit
+                </button>
+                <button className="w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out">
+                    Registrovat
+                </button>
+            </div>
 
-                {/* Visible to logged in user */}
-                <div className="flex flex-col h-full w-32 justify-between items-center pl-2 pr-2 border-l-2 border-black hidden">
-                    <a className="inline-flex w-full h-full items-center justify-center border-b-2 border-black cursor-pointer">
-                        Wishlist
-                    </a>
-                    <a className="inline-flex w-full h-full items-center justify-center cursor-pointer">
-                        Odhlásit
-                    </a>
-                </div>
-            </header>
+            {/* Visible to logged in user */}
+            <div className="flex flex-col h-full justify-between items-center pl-2 pr-2 border-l-2 border-gray-700 hidden">
+                <button className="w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out">
+                    Wishlist
+                </button>
+                <button className="w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out">
+                    Odhlásit
+                </button>
+            </div>
         </div>
-  )
+    )
 }
