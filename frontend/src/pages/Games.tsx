a96c328220dc26727f2f7544c6986f59a70b10c7
Jan Nouza <xnouza@fi.muni.cz>
2023-06-01 14:44:01 +0200

    feat(Games): Games page done

--- frontend/src/pages/Games.tsx
+++ frontend/src/pages/Games.tsx
@@ -1,5 +1,104 @@
+import { useState } from 'react';
+import games from '../assets/games.json';
+import developers from '../assets/developers.json';
+import genres from '../assets/genres.json';
+import { GameItem } from '../components/GameItem';
+
 export const Games = () => {
+  const [filter, setFilter] = useState('');
+  const [yearFilter, setYearFilter] = useState('');
+  const [developerFilter, setDeveloperFilter] = useState('');
+  const [genreFilter, setGenreFilter] = useState('');
+  const [sortType, setSortType] = useState('name-asc');
+
+  let filteredGames = games.filter(game => {
+    const matchesName = game.name.toLowerCase().includes(filter.toLowerCase());
+    const matchesYear = game.releaseDate.includes(yearFilter);
+    const matchesDeveloper = developerFilter === '' || game.developers.includes(developerFilter);
+    const matchesGenre = genreFilter === '' || game.genres.includes(genreFilter);
+    return matchesName && matchesYear && matchesDeveloper && matchesGenre;
+  });
+
+  filteredGames.sort((a, b) => {
+    if (sortType === 'name-asc') {
+      return a.name.localeCompare(b.name);
+    } else if (sortType === 'name-desc') {
+      return b.name.localeCompare(a.name);
+    } else if (sortType === 'year-asc') {
+      return a.releaseDate.localeCompare(b.releaseDate);
+    } else if (sortType === 'year-desc') {
+      return b.releaseDate.localeCompare(a.releaseDate);
+    } else {
+      return 0;
+    }
+  });
+
   return (
-    <div>Games</div>
-  )
-}
+    <div>
+      <div className="p-4 bg-white shadow rounded-lg mb-6">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-5">
+          <input
+            type="text"
+            placeholder="Jméno hry"
+            value={filter}
+            onChange={e => setFilter(e.target.value)}
+            className="p-2 border-2 border-gray-300 rounded"
+          />
+
+          <select
+            value={developerFilter}
+            onChange={e => setDeveloperFilter(e.target.value)}
+            className="p-2 border-2 border-gray-300 rounded"
+          >
+            <option value="">Všichni vývojáři</option>
+            {developers.map(dev => (
+              <option key={dev.id} value={dev.id}>{dev.name}</option>
+            ))}
+          </select>
+
+          <select
+            value={genreFilter}
+            onChange={e => setGenreFilter(e.target.value)}
+            className="p-2 border-2 border-gray-300 rounded"
+          >
+            <option value="">Všechny žánry</option>
+            {genres.map(gen => (
+              <option key={gen.id} value={gen.id}>{gen.type}</option>
+            ))}
+          </select>
+
+          <input
+            type="text"
+            placeholder="Datum vydání (YYYY-MM-DD)"
+            value={yearFilter}
+            onChange={e => setYearFilter(e.target.value)}
+            className="p-2 border-2 border-gray-300 rounded"
+          />
+
+          <select
+            value={sortType}
+            onChange={e => setSortType(e.target.value)}
+            className="p-2 border-2 border-gray-300 rounded"
+          >
+
+            <option value="name-asc">Abecedně (A-Z)</option>
+            <option value="name-desc">Abecedně (Z-A)</option>
+            <option value="year-asc">Dle vydání (od nejstarších)</option>
+            <option value="year-desc">Dle vydání (od nejnovějších)</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 p-4">
+        {filteredGames.map(game => (
+          <GameItem
+            key={game.id}
+            game={game}
+            developers={developers}
+            genres={genres}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
