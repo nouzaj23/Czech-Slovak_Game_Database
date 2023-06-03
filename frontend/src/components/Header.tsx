b9cce4d6d71f19f68bcf215e10966722e3bd05f2
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-03 11:44:11 +0200

    fix(mobile_version_menu): removed unused import

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
