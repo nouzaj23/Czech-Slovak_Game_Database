a87504b77ca15be5977145dc51ebe279bc23adda
Jan Nouza <xnouza@fi.muni.cz>
2023-06-04 09:08:11 +0200

    fix(GameCard): Removing comments and adding Link

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -2,27 +2,24 @@ import { Game } from "../models";
 import developers from '../assets/developers.json';
 import genres from '../assets/genres.json';
 import reviews from '../assets/reviews.json';
+import { Link } from "react-router-dom";
 
 type GameCardProps = Game;
 
 export const GameCard: React.FC<GameCardProps> = ({ ...game }) => {
     let gameDevelopers = developers.filter(developer => game.developers.includes(developer.id));
     let gameGenres = genres.filter(genre => game.genres.includes(genre.id));
-
-    // Filter reviews for this game
     let gameReviews = reviews.filter(review => review.game === game.id);
 
-    // Calculate the average rating
     let avgRating = 0;
     if (gameReviews.length > 0) {
         let totalRating = gameReviews.reduce((total, review) => total + review.rating, 0);
         avgRating = totalRating / gameReviews.length;
     }
 
-    // Determine the color of the rating based on its value
     const ratingColor = avgRating >= 8 ? 'text-green-500'
-                        : avgRating >= 6 ? 'text-yellow-500'
-                        : 'text-red-500';
+        : avgRating >= 6 ? 'text-yellow-500'
+            : 'text-red-500';
 
     return (
         <div className='flex flex-col w-1/2 h-full bg-white border-2 border-black rounded-xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 ease-in-out p-4 m-4'>
@@ -31,7 +28,9 @@ export const GameCard: React.FC<GameCardProps> = ({ ...game }) => {
                     <img src={game.cover} className='mx-auto my-2 object-cover rounded-md shadow-md' alt={game.name} />
                 </div>
                 <div className='flex flex-col w-auto pt-3 gap-1 pl-4'>
-                    <h1 className='text-2xl font-bold text-gray-700'>{game.name}</h1>
+                    <Link to={`games/${game.id}`}>
+                        <h1 className='text-2xl font-bold text-gray-700'>{game.name}</h1>
+                    </Link>
                     <div className='text-gray-500'>
                         {gameDevelopers.map((developer, index) => {
                             return (
