55b4d4013ff609756e8a005483df8f85c976d5da
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-04 21:37:01 +0200

    fix(gameitem): fixed format of file

--- frontend/src/components/GameItem.tsx
+++ frontend/src/components/GameItem.tsx
@@ -49,39 +49,38 @@ export const GameItem: React.FC<GameItemProps> = ({ game, developers, genres, re
 
     return (
         <div className="rounded overflow-hidden shadow-lg flex flex-col transform transition duration-500 ease-in-out hover:shadow-2xl hover:scale-105">
-    <div className="w-full h-48 bg-gray-300 flex justify-center">
-        <img className="w-2/3 h-full object-cover" src={game.cover} alt="Game cover" />
-    </div>
-    <div className="px-6 py-4 flex-grow">
-        <Link to={`games/${game.id}`}>
-            <div className="font-bold text-xl mb-2">{game.name}</div>
-        </Link>
-        <p className="text-gray-700 text-base">
-            {game.description}
-        </p>
-    </div>
-    <div className="px-6 pt-4 pb-2 flex items-center">
-        <div className="flex-grow">
-            <p className="text-gray-700 text-sm">
-                <b>Datum vydání:</b> {game.releaseDate}
-            </p>
-            <p className="text-gray-700 text-sm">
-                <b>Vývojáři:</b> {getDeveloperNames(game.developers)}
-            </p>
-            <p className="text-gray-700 text-sm">
-                <b>Žánry:</b> {getGenreNames(game.genres)}
-            </p>
-        </div>
-        <div className="flex items-center">
-            <div>
-                <FontAwesomeIcon icon={faHeart} size='2x' className='text-red-500' />
+            <div className="w-full h-48 bg-gray-300 flex justify-center">
+                <img className="w-2/3 h-full object-cover" src={game.cover} alt="Game cover" />
             </div>
-            <div className="ml-2 text-white rounded-full px-3 py-1 shadow-md" style={{ background: ratingBg() }}>
-                <p className="font-bold text-lg">{rating.toFixed(1)}</p>
+            <div className="px-6 py-4 flex-grow">
+                <Link to={`games/${game.id}`}>
+                    <div className="font-bold text-xl mb-2">{game.name}</div>
+                </Link>
+                <p className="text-gray-700 text-base">
+                    {game.description}
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2 flex items-center">
+                <div className="flex-grow">
+                    <p className="text-gray-700 text-sm">
+                        <b>Datum vydání:</b> {game.releaseDate}
+                    </p>
+                    <p className="text-gray-700 text-sm">
+                        <b>Vývojáři:</b> {getDeveloperNames(game.developers)}
+                    </p>
+                    <p className="text-gray-700 text-sm">
+                        <b>Žánry:</b> {getGenreNames(game.genres)}
+                    </p>
+                </div>
+                <div className="flex items-center">
+                    <div>
+                        <FontAwesomeIcon icon={faHeart} size='2x' className='text-red-500' />
+                    </div>
+                    <div className="ml-2 text-white rounded-full px-3 py-1 shadow-md" style={{ background: ratingBg() }}>
+                        <p className="font-bold text-lg">{rating.toFixed(1)}</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
     )
 };
