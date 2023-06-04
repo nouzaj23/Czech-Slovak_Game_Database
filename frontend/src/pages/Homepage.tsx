591befb32edfd82388fc5166c6002512822c8230
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-04 18:51:47 +0200

    fix(homepage): fixed width of cards

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -9,22 +9,33 @@ export const Homepage = () => {
     return (
         <div className="flex flex-col h-full w-full p-8 bg-gray-100">
             <h1 className="text-4xl font-semibold mb-8 text-gray-700">Poslední vydané hry:</h1>
-            <div className='flex flex-row justify-between items-center gap-6 mb-8'>
-                <div className="w-3/4 mx-auto p-4 bg-white shadow rounded">
-                    <GameCard {...recentGames[0]} />
+            <div className='flex flex-row justify-between items-stretch gap-6 mb-8'>
+                <div className="w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto">
+                        <GameCard {...recentGames[0]} />
+                    </div>
                 </div>
-                <div className="w-3/4 mx-auto p-4 bg-white shadow rounded">
-                    <GameCard {...recentGames[1]} />
-                </div>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-6'>
-                <div className="w-3/4 mx-auto p-4 bg-white shadow rounded">
-                    <GameCard {...recentGames[2]} />
-                </div>
-                <div className="w-3/4 mx-auto p-4 bg-white shadow rounded">
-                    <GameCard {...recentGames[3]} />
+                <div className="w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto">
+                        <GameCard {...recentGames[1]} />
+                    </div>
                 </div>
             </div>
+            <div className='flex flex-row justify-between items-stretch gap-6'>
+                <div className="w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto">
+                        <GameCard {...recentGames[2]} />
+                    </div>
+                </div>
+                <div className="w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto">
+                        <GameCard {...recentGames[3]} />
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
     );
 }
