de808b854428a0b0f94964d39805141974085602
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-04 18:07:20 +0200

    feat(gamepage): info about game was moved to the gamecard component

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,17 +1,14 @@
 import { useState } from 'react';
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import games from '../assets/games.json';
-import developers from '../assets/developers.json';
-import genres from '../assets/genres.json';
 import reviews from '../assets/reviews.json';
 import { ReviewItem } from '../components/Review';
 import { AddReviewForm } from '../components/AddReviewForm'
 import { CommentItem } from '../components/CommentItem';
 import { AddCommentForm } from '../components/AddCommentForm';
-import { Developer, Genre, Game } from '../models';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Game } from '../models';
 import ReactPlayer from 'react-player';
+import { GameCard } from '../components/GameCard';
 
 
 export const GamePage = () => {
@@ -24,10 +21,7 @@ export const GamePage = () => {
         return <div>Hra není k dispozici</div>;
     }
 
-    const developersCopy: Developer[] = developers.filter(developer => game.developers.includes(developer.id));
-    const genresCopy: Genre[] = genres.filter(genre => game.genres.includes(genre.id));
     const rating: number = reviews.filter(review => game.reviews.includes(review.id)).reduce((accumulator, currentValue) => accumulator + currentValue.rating, 0) / game.reviews.length;
-
 
     const [selectedTab, setSelectedTab] = useState('reviews');
 
@@ -98,7 +92,7 @@ export const GamePage = () => {
                         <button onClick={handleAddReview} style={{ background: ratingBg() }} className="px-3 py-2 mb-5 mt-5 text-white rounded">Přidat recenzi</button>
                         <div>
                             {isOpen && (
-                                <AddReviewForm />
+                                <AddReviewForm gameId={game.id} game={game} />
                             )}
                         </div>
                         <div className="review-list space-y-4">
@@ -160,24 +154,7 @@ export const GamePage = () => {
 
     return (
         <div className="w-3/4 mx-auto p-4 bg-white shadow rounded">
-            <div className="flex bg-white shadow-lg rounded-lg p-6 relative">
-                <img src={game.cover} alt="popis" className="w-1/4 rounded-lg" />
-                <div className="ml-4 w-3/4">
-                    <h2 className="text-xl font-bold">{game.name}</h2>
-                    <p className="mt-2 text-gray-600">Developers: {developersCopy.map((developer, index) => <Link to="" key={index} className="text-blue-500 hover:underline">{developer.name}{index !== developersCopy.length - 1 && ', '}</Link>)}</p>
-                    <p className="mt-2 text-gray-600">Release Date: {game.releaseDate}</p>
-                    <p className="mt-2 text-gray-600">Genres: {genresCopy.map((genre, index) => <Link to="" key={index} className="text-blue-500 hover:underline">{genre.type}{index !== genresCopy.length - 1 && ', '}</Link>)}</p>
-                    <p className="mt-4 text-gray-700">
-                        {game.description}
-                    </p>
-                </div>
-                <div className="absolute top-2 right-2 text-white rounded-full px-3 py-1 shadow-md" style={{ background: ratingBg() }}>
-                    <p className="font-bold text-lg">{rating.toFixed(1)}</p>
-                </div>
-                <div className="absolute top-12 right-4">
-                    <FontAwesomeIcon icon={faHeart} size='2x' className='text-red-500' />
-                </div>
-            </div>
+            <GameCard {...game}/>
 
             <div className="mt-4 flex justify-around gap-4">
                 <button onClick={() => setSelectedTab('reviews')} style={{ background: ratingBg() }} className="px-3 py-2 text-white w-full hover:bg-blue-600 transition-all duration-300 ease-in-out shadow-md hover:shadow-lg">
