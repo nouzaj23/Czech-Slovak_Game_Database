a4e9d16d410a46d9b57097446ea607147bc6186b
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-04 21:39:22 +0200

    fix(gameitem): deleted unused code

--- frontend/src/components/GameItem.tsx
+++ frontend/src/components/GameItem.tsx
@@ -37,11 +37,11 @@ export const GameItem: React.FC<GameItemProps> = ({ game, developers, genres, re
         }).join(', ');
     };
 
-    let avgRating = 0;
-    if (reviews.length > 0) {
-        let totalRating = reviews.reduce((total, review) => total + review.rating, 0);
-        avgRating = totalRating / reviews.length;
-    }
+    // let avgRating = 0;
+    // if (reviews.length > 0) {
+    //     let totalRating = reviews.reduce((total, review) => total + review.rating, 0);
+    //     avgRating = totalRating / reviews.length;
+    // }
 
     return (
         <div className="rounded overflow-hidden shadow-lg flex flex-col transform transition duration-500 ease-in-out hover:shadow-2xl hover:scale-105">
