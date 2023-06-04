5de90d3f57a06955930b8ccb49154f817e6a3462
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-04 21:36:17 +0200

    feat(gameitem): upgraded gameitem - added heart and rating

--- frontend/src/components/GameItem.tsx
+++ frontend/src/components/GameItem.tsx
@@ -1,5 +1,7 @@
 import { Game, Developer, Genre, Review } from '../models';
 import { Link } from 'react-router-dom';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface GameItemProps {
     game: Game;
@@ -15,6 +17,18 @@ export const GameItem: React.FC<GameItemProps> = ({ game, developers, genres, re
             return developer ? developer.name : '';
         }).join(', ');
     };
+
+    const rating: number = reviews.filter(review => game.reviews.includes(review.id)).reduce((accumulator, currentValue) => accumulator + currentValue.rating, 0) / game.reviews.length;
+
+    const ratingBg = () => {
+        if (rating > 7) {
+            return '#ad0e30';
+        }
+        else if (rating > 3) {
+            return '#3690eb';
+        }
+        return '#010203';
+    }
 
     const getGenreNames = (genreIds: string[]) => {
         return genreIds.map(id => {
@@ -35,31 +49,39 @@ export const GameItem: React.FC<GameItemProps> = ({ game, developers, genres, re
 
     return (
         <div className="rounded overflow-hidden shadow-lg flex flex-col transform transition duration-500 ease-in-out hover:shadow-2xl hover:scale-105">
-            <div className="w-full h-48 bg-gray-300 flex justify-center">
-                <img className="w-2/3 h-full object-cover" src={game.cover} alt="Game cover" />
+    <div className="w-full h-48 bg-gray-300 flex justify-center">
+        <img className="w-2/3 h-full object-cover" src={game.cover} alt="Game cover" />
+    </div>
+    <div className="px-6 py-4 flex-grow">
+        <Link to={`games/${game.id}`}>
+            <div className="font-bold text-xl mb-2">{game.name}</div>
+        </Link>
+        <p className="text-gray-700 text-base">
+            {game.description}
+        </p>
+    </div>
+    <div className="px-6 pt-4 pb-2 flex items-center">
+        <div className="flex-grow">
+            <p className="text-gray-700 text-sm">
+                <b>Datum vydání:</b> {game.releaseDate}
+            </p>
+            <p className="text-gray-700 text-sm">
+                <b>Vývojáři:</b> {getDeveloperNames(game.developers)}
+            </p>
+            <p className="text-gray-700 text-sm">
+                <b>Žánry:</b> {getGenreNames(game.genres)}
+            </p>
+        </div>
+        <div className="flex items-center">
+            <div>
+                <FontAwesomeIcon icon={faHeart} size='2x' className='text-red-500' />
             </div>
-            <div className="px-6 py-4 flex-grow">
-                <Link to={`games/${game.id}`}>
-                    <div className="font-bold text-xl mb-2">{game.name}</div>
-                </Link>
-                <p className="text-gray-700 text-base">
-                    {game.description}
-                </p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-                <p className="text-gray-700 text-sm">
-                    <b>Datum vydání:</b> {game.releaseDate}
-                </p>
-                <p className="text-gray-700 text-sm">
-                    <b>Vývojáři:</b> {getDeveloperNames(game.developers)}
-                </p>
-                <p className="text-gray-700 text-sm">
-                    <b>Žánry:</b> {getGenreNames(game.genres)}
-                </p>
-                <p className={`text-sm ${ratingColor}`}>
-                    <b className='text-black'>Hodnocení:</b> {avgRating.toFixed(2)}
-                </p>
+            <div className="ml-2 text-white rounded-full px-3 py-1 shadow-md" style={{ background: ratingBg() }}>
+                <p className="font-bold text-lg">{rating.toFixed(1)}</p>
             </div>
         </div>
+    </div>
+</div>
+
     )
 };
