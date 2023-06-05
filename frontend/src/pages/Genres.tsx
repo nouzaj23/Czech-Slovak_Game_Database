022c3a98d7ce465dbdc1b1237845f9fba8af39d4
Jan Nouza <xnouza@fi.muni.cz>
2023-06-05 18:31:23 +0200

    fix(Genres): Removing unnecessary classname

--- frontend/src/pages/Genres.tsx
+++ frontend/src/pages/Genres.tsx
@@ -4,7 +4,7 @@ import genres from '../assets/genres.json';
 export const Genres = () => {
   return (
     <div className="p-4 bg-white shadow rounded-lg mb-6">
-      <div className="flex flex-wrap -m-4">
+      <div className="flex flex-wrap">
         {genres.map(genre => (
           <Link to={`/games?genre=${genre.id}`} key={genre.id} className="w-full sm:w-1/2 md:w-1/3 p-4">
             <div className="flex flex-col justify-between bg-gray-200 rounded-lg p-4 hover:bg-gray-300 transition-colors duration-300 ease-in-out cursor-pointer h-full">
