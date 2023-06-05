bef354304e78038258b0968aed89c3d80884dc64
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-05 16:31:49 +0200

    feat(homepage): adapted for small devices

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -9,33 +9,30 @@ export const Homepage = () => {
     return (
         <div className="flex flex-col h-full w-full p-8 bg-gray-100">
             <h1 className="text-4xl font-semibold mb-8 text-gray-700">Poslední vydané hry:</h1>
-            <div className='flex flex-row justify-between items-stretch gap-6 mb-8'>
-                <div className="w-1/2 p-4 bg-white shadow rounded">
+            <div className='flex flex-col md:flex-row justify-between items-stretch gap-6 mb-8'>
+                <div className="w-full md:w-1/2 p-4 bg-white shadow rounded">
                     <div className="flex-auto">
                         <GameCard {...recentGames[0]} />
                     </div>
                 </div>
-                <div className="w-1/2 p-4 bg-white shadow rounded">
+                <div className="w-full md:w-1/2 p-4 bg-white shadow rounded">
                     <div className="flex-auto">
                         <GameCard {...recentGames[1]} />
                     </div>
                 </div>
             </div>
-            <div className='flex flex-row justify-between items-stretch gap-6'>
-                <div className="w-1/2 p-4 bg-white shadow rounded">
+            <div className='flex flex-col md:flex-row justify-between items-stretch gap-6'>
+                <div className="w-full md:w-1/2 p-4 bg-white shadow rounded">
                     <div className="flex-auto">
                         <GameCard {...recentGames[2]} />
                     </div>
                 </div>
-                <div className="w-1/2 p-4 bg-white shadow rounded">
+                <div className="w-full md:w-1/2 p-4 bg-white shadow rounded">
                     <div className="flex-auto">
                         <GameCard {...recentGames[3]} />
                     </div>
                 </div>
             </div>
-
-
-
         </div>
     );
 }
