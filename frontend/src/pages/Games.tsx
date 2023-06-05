d988bb7b7f7221a67b34f6f3a2186b917ae7800a
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-05 19:12:10 +0200

    feat(games): solved small screens

--- frontend/src/pages/Games.tsx
+++ frontend/src/pages/Games.tsx
@@ -104,7 +104,7 @@ export const Games = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-4 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         {filteredGames.map(game => (
           <GameItem
             key={game.id}
