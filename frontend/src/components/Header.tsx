aa978925789c5400e0679bd5545a5cc5e2751dad
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-06 16:20:17 +0200

    feat(login): added login window

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { LoginForm } from "./LoginForm";
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
     return (
-        <div style={{zIndex: "10"}} className="flex flex-col sm:flex-row w-full h-auto sm:h-16 fixed top-0 bg-gray-800 text-white justify-between items-center px-4 shadow-md">
+        <div style={{ zIndex: "10" }} className="flex flex-col sm:flex-row w-full h-auto sm:h-16 fixed top-0 bg-gray-800 text-white justify-between items-center px-4 shadow-md">
             <div className="flex flex-row justify-between w-full sm:w-auto">
                 <Link to="/">
                     <img src="./../../logo-placeholder.png" className="h-10 w-auto mr-2 mt-3 sm:mt-0 mb-3 sm:mb-0" />
@@ -30,8 +32,6 @@ export const Header = () => {
                 </div>
             </div>
 
-
-
             <div className={`${isOpen ? 'flex' : 'hidden'} sm:flex flex-col sm:flex-row justify-center items-center w-full sm:w-1/3 mt-2 sm:mt-0 bg-transparent`}>
                 <Link reloadDocument to="/games" className="w-full text-center">
                     <button className="sm:w-auto w-full px-3 py-1 rounded-md text-white sm:bg-gray-700 sm:hover:bg-gray-600 transition-colors duration-300 ease-in-out">
@@ -52,11 +52,12 @@ export const Header = () => {
 
             <div className="">
                 <div className={`${isOpen ? 'flex' : 'hidden'} sm:flex flex-col h-full justify-between w-full sm:w-auto items-center pl-2 pr-2 ${isOpen ? '' : 'sm:border-l-0'} mb-2 sm:mb-0`}>
-                    <Link to="/login">
-                        <button className="sm:w-auto py-1 sm:py-0 w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out">
-                            Přihlásit
-                        </button>
-                    </Link>
+                    <button className="sm:w-auto py-1 sm:py-0 w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out" onClick={() => setShowLogin(true)}>Login</button>
+                    {showLogin && (
+                        <div className="fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                            <LoginForm handleClose={() => setShowLogin(false)} />
+                        </div>
+                    )}
                     <Link to="/register">
                         <button className="sm:w-auto py-1 sm:py-0 w-full h-full flex items-center justify-center hover:text-gray-300 transition-colors duration-300 ease-in-out">
                             Registrovat
---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,33 @@
+import {  MouseEventHandler } from 'react';
+
+
+interface LoginFormProps {
+    handleClose: MouseEventHandler;
+}
+
+export const LoginForm: React.FC<LoginFormProps> = ({ handleClose }) => {
+    //   const handleSubmit = () => {
+    //      tady prijde backend sracky
+    //   };
+
+    return (
+        <form className="p-6 bg-white rounded shadow-md">
+            <div className="mb-4">
+                <label className="block text-sm font-bold mb-2">
+                    Nickname
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="nickname" placeholder="nickname" required />
+                </label>
+            </div>
+            <div className="mb-6">
+                <label className="block text-sm font-bold mb-2">
+                    Password
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" name="password" placeholder="password" required />
+                </label>
+            </div>
+            <div className="flex items-center justify-between">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleClose}>X</button>
+            </div>
+        </form>
+    );
+};
