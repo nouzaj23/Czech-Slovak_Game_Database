069ba3148823c09479dd91acf4eaec37a61850a8
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-19 23:57:53 +0200

    fix(cshd): type error

--- frontend/src/pages/Genres.tsx
+++ frontend/src/pages/Genres.tsx
@@ -8,7 +8,7 @@ export const Genres = () => {
         {genres.map(genre => (
           <Link to={`/games?genre=${genre.id}`} key={genre.id} className="w-full sm:w-1/2 md:w-1/3 p-4">
             <div className="flex flex-col justify-between bg-gray-200 rounded-lg p-4 hover:bg-gray-300 transition-colors duration-300 ease-in-out cursor-pointer h-full">
-              <h2 className="font-bold text-xl mb-2">{genre.type}</h2>
+              <h2 className="font-bold text-xl mb-2">{genre.name}</h2>
               <p className="flex-grow">{genre.description}</p>
             </div>
           </Link>
