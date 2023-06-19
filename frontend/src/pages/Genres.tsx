7bc36d16def542c769341851f6449e5f3292bb7e
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-20 00:01:39 +0200

    fix(genres): genres loading fixed

--- frontend/src/pages/Genres.tsx
+++ frontend/src/pages/Genres.tsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
-import genres from '../assets/genres.json';
+import { Genre } from "../models";
+import { useEffect, useState } from "react";
+import { GenreApi } from "../services";
 
 export const Genres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            setGenres(await GenreApi.retrieveAllGenres());
+        }
+        catch (error) {
+            console.log("Games was not loaded");
+        }
+    }
+    fetchData();
+}, []);
+
   return (
     <div className="p-4 bg-white shadow rounded-lg mb-6">
       <div className="flex flex-wrap">
