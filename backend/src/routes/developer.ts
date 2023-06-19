0318fa32e089df682c8a294bd118e7b2166b3dbf
Tomáš Rohlínek <tomas.rohlinek@outlook.cz>
2023-06-19 11:07:05 +0200

    feat: implement developer endpoint

---
+++ backend/src/routes/developer.ts
@@ -0,0 +1,52 @@
+import { Context } from '@/context'
+
+import Express from 'express'
+import { makeUserAuthMiddleware } from '@/middleware'
+import { NotLoggedIn, SystemError } from '@/errors'
+
+export function makeRouter(context: Context) {
+  const router = Express.Router({ mergeParams: true })
+
+  router.route('/')
+    .get(async (req, res, next) => {
+      try {
+        const developers = await context.controllers.developer.readMultiple({...req.params, ...req.body}, req.session.auth?.userId)
+        res.json(developers)
+      } catch (error) {
+        next(error)
+      }
+    })
+    .post(makeUserAuthMiddleware((req) => req.body.userId))
+    .post(async (req, res, next) => {
+      try {
+        const developer = await context.controllers.developer.create({...req.params, ...req.body}, req.session.auth?.userId)
+        res.json(developer)
+      } catch (error) {
+        next(error)
+      }
+    })
+
+  router.route('/:developerId')
+    .get(async (req, res, next) => {
+      try {
+        const developer = await context.controllers.developer.readSingle({...req.params, ...req.body}, req.session.auth?.userId)
+        res.json(developer)
+      } catch (error) {
+        next(error)
+      }
+    })
+    .patch(async (req, res, next) => {
+      try {
+        const userId = req.session.auth?.userId
+        if (!userId)
+          throw new NotLoggedIn()
+
+        const developer = await context.controllers.developer.update({...req.params, ...req.body}, req.session.auth?.userId)
+        res.json(developer)
+      } catch (error) {
+        next(error)
+      }
+    })
+
+  return router
+}
