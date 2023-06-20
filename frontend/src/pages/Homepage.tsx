3a8cba224c87968f747c2d5de5c5c774917a1cb2
Jan Nouza <xnouza@fi.muni.cz>
2023-06-20 11:34:59 +0200

    fix(Homepage): Revert to old design

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -17,15 +17,30 @@ export const Homepage = () => {
     return (
         <div className="flex flex-col h-full w-full p-8 bg-gray-100">
             <h1 className="text-4xl font-semibold mb-8 text-gray-700">Poslední vydané hry:</h1>
-            {recentGames.map((game, index) => (
-                <div key={index} className='flex flex-col md:flex-row justify-between items-stretch gap-6 mb-8'>
-                    <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
-                        <div className="flex-auto flex-grow">
-                            <GameCard game={game}/>
-                        </div>
+            <div className='flex flex-col md:flex-row justify-between items-stretch gap-6 mb-8'>
+                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto flex-grow">
+                        <GameCard game={recentGames[0]}/>
                     </div>
                 </div>
-            ))}
+                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto flex-grow">
+                        <GameCard game={recentGames[1]}/>
+                    </div>
+                </div>
+            </div>
+            <div className='flex flex-col md:flex-row justify-between items-stretch gap-6'>
+                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto flex-grow">
+                        <GameCard game={recentGames[2]}/>
+                    </div>
+                </div>
+                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
+                    <div className="flex-auto flex-grow">
+                        <GameCard game={recentGames[3]}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
