4f541db94470efed8f8d6c18803330b2ed0b2f0f
Jan Nouza <xnouza@fi.muni.cz>
2023-06-20 11:05:16 +0200

    fix(recentGamesSelector): Sort copy of array

--- frontend/src/state/selectors.ts
+++ frontend/src/state/selectors.ts
@@ -6,7 +6,7 @@ export const recentGamesSelector = selector<Game[]>({
     key: 'recentGames',
     get: ({ get }) => {
       const games = get(gamesAtom);
-      const sortedGames = games.sort((a, b) => new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime());
+      const sortedGames = [...games].sort((a, b) => new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime());
       return sortedGames.slice(0, 4);
     }
   });
