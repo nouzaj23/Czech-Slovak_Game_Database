590a4eeb8cad9801e87176b86a36141f98545de0
Jan Nouza <xnouza@fi.muni.cz>
2023-06-20 11:20:09 +0200

    fix(GameCard): Release date format

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -45,7 +45,7 @@ export const GameCard: React.FC<GameCardProps> = ({ game }) => {
                     <h2 className="pr-10 text-xl font-bold hover:underline">{game.name}</h2>
                 </Link>
                 <p className="pr-10 mt-2 text-gray-600"><b>Vývojáři:</b> {game.developers?.map((developer, index) => <Link to={`/developers/${developer.id}`} key={index} className="text-blue-500 hover:underline">{developer.name}{index !== game.developers.length - 1 && ', '}</Link>)}</p>
-                <p className="mt-2 text-gray-600"><b>Datum vydání:</b> {new Date(game.releaseDate).toLocaleDateString()}</p>
+                <p className="mt-2 text-gray-600"><b>Datum vydání:</b> {new Date(game.releaseDate).toISOString().slice(0,10)}</p>
                 <p className="mt-2 text-gray-600"><b>Žánry:</b> {game.genres?.map((genre, index) => <Link to={`/games?genre=${genre.id}`} key={index} className="text-blue-500 hover:underline">{genre.name}{index !== game.genres.length - 1 && ', '}</Link>)}</p>
                 <p className="mt-4 text-gray-700">
                     {game.description}
