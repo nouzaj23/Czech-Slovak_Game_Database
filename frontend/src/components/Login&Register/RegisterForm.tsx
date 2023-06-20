3e6154f82a1189fe2225de66034996286bd1ca9d
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-20 17:30:17 +0200

    fix(register): fixed register

--- frontend/src/components/Login&Register/RegisterForm.tsx
+++ frontend/src/components/Login&Register/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { FormEventHandler, MouseEventHandler, useCallback } from 'react';
+import { FormEventHandler, MouseEventHandler, useCallback, useState } from 'react';
 import useLogin from '../../hooks/useLogin';
 import { UserApi } from '../../services';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -8,45 +8,43 @@ interface RegisterFormProps {
 }
 
 export const RegisterForm: React.FC<RegisterFormProps> = ({ handleClose }) => {
-    const { login } = useLogin({ redirect: '/' })
+    const { login } = useLogin({ redirect: '/' });
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(async (e) => {
-        // const { data: usersData } = useQuery<User[]>(['users'], UserApi.retrieveAllUsers);
-        // const users = usersData ?? [];
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation(() => UserApi.register(username, password, email), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['users']);
+            login({password: password, username: username});
+        },
+    });
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(async (e) => {
         e.preventDefault();
-        const username = document.getElementById("nickname") as HTMLInputElement;
-        const password = document.getElementById("password") as HTMLInputElement;
-        const confirmPassword = document.getElementById("confirmPassword") as HTMLInputElement;
-        const email = document.getElementById("email") as HTMLInputElement;
-        const errorLabel = document.getElementById("error") as HTMLLabelElement;
-        if (username != undefined && password != undefined && email != undefined) {
-            if (password.value !== confirmPassword.value) {
-                errorLabel.textContent = "Hesla se neshodují";
-            }
-            else if (!email.value.match(emailPattern)) {
-                errorLabel.textContent = "E-mail je v nesprávném formátu";
-            }
-            else if (password.value.length < 8) {
-                errorLabel.textContent = "Heslo je příliš krátké";
-            }
-            // else if (users.some((user: User) => user.username == username.value)) {
-            //     errorLabel.textContent = "Uživatel s touto přezdívkou existuje";
-            // }
-            else {
-                console.log("zacatek registrace");
-                const queryClient = useQueryClient();
-                useMutation({
-                    mutationFn: () => UserApi.register(username.value, password.value, email.value),
-                    onSuccess: () => {
-                        queryClient.invalidateQueries(['users']);
-                    },
-                });
-                console.log("OK");
-            }
+        setUsername((document.getElementById("nickname") as HTMLInputElement).value);
+        setPassword((document.getElementById("password") as HTMLInputElement).value);
+        setConfirmPassword((document.getElementById("confirmPassword") as HTMLInputElement).value);
+        setEmail((document.getElementById("email") as HTMLInputElement).value);
+        if (password !== confirmPassword) {
+            setErrorMessage("Hesla se neshodují");
         }
-    }, [login]);
+        else if (!email.match(emailPattern)) {
+            setErrorMessage("E-mail je v nesprávném formátu");
+        }
+        else if (password.length < 8) {
+            setErrorMessage("Heslo je příliš krátké");
+        }
+        else {
+            mutation.mutate();
+        }
+    }, [email, username, password, confirmPassword, mutation]);
 
 
     return (
@@ -82,7 +80,7 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({ handleClose }) => {
                 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleClose}>X</button>
             </div>
             <div className="mb-4 flex items-center">
-                <label id="error"></label>
+                <label id="error">{errorMessage}</label>
             </div>
         </form>
     );
