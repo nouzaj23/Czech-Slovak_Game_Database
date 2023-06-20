cffafb3672c8b54d176f805e32a7145a87c4937c
Jan Nouza <xnouza@fi.muni.cz>
2023-06-20 11:32:10 +0200

    fix: Removing Recoil and updating Homepage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RecoilRoot } from 'recoil';
 import './index.css';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </RecoilRoot>
   </React.StrictMode>
 );
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,51 +1,31 @@
 import { Game } from '../models';
 import { GameCard } from '../components/GameCard';
-import { useEffect } from 'react';
 import { GameApi } from '../services';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { gamesAtom } from '../state/atoms';
 import { useQuery } from '@tanstack/react-query';
-import { recentGamesSelector } from '../state/selectors';
 
 export const Homepage = () => {
-    const setGames = useSetRecoilState(gamesAtom)
     const { data: games } = useQuery<Game[]>(['games'], GameApi.retrieveAllGames);
 
-    useEffect(() => {
-        if (games) {
-            setGames(games);
-        }
-    }, [games, setGames]);
+    let recentGames: Game[] = [];
 
-    const recentGames = useRecoilValue(recentGamesSelector);
+    if (games) {
+        recentGames = [...games].sort((a, b) => {
+            return new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime();
+        }).slice(0, 4);
+    }
 
     return (
         <div className="flex flex-col h-full w-full p-8 bg-gray-100">
             <h1 className="text-4xl font-semibold mb-8 text-gray-700">Poslední vydané hry:</h1>
-            <div className='flex flex-col md:flex-row justify-between items-stretch gap-6 mb-8'>
-                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
-                    <div className="flex-auto flex-grow">
-                        <GameCard game={recentGames[0]}/>
+            {recentGames.map((game, index) => (
+                <div key={index} className='flex flex-col md:flex-row justify-between items-stretch gap-6 mb-8'>
+                    <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
+                        <div className="flex-auto flex-grow">
+                            <GameCard game={game}/>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
-                    <div className="flex-auto flex-grow">
-                        <GameCard game={recentGames[1]}/>
-                    </div>
-                </div>
-            </div>
-            <div className='flex flex-col md:flex-row justify-between items-stretch gap-6'>
-                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
-                    <div className="flex-auto flex-grow">
-                        <GameCard game={recentGames[2]}/>
-                    </div>
-                </div>
-                <div className="flex flex-col w-full md:w-1/2 p-4 bg-white shadow rounded">
-                    <div className="flex-auto flex-grow">
-                        <GameCard game={recentGames[3]}/>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     );
 }
