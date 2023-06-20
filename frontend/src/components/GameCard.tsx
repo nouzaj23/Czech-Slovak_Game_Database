ed5bc064cbc84dd4b3af115b1e42cb9efad76960
Jan Nouza <xnouza@fi.muni.cz>
2023-06-20 11:17:24 +0200

    fix(GameCard): Cosmetic changes

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -38,14 +38,14 @@ export const GameCard: React.FC<GameCardProps> = ({ game }) => {
 
     return (
         <div className="p-6 relative" style={{ overflow: 'auto' }}>
-            <img src={game.cover != undefined ? game.cover : ""} alt="popis" className="w-1/4 object-contain object-top float-left mr-4" />
+            <img src={game.cover != undefined ? game.cover : ""} alt="Obálka hry" className="w-1/4 object-contain object-top float-left mr-4" />
 
             <div>
                 <Link to={`/games/${game.id}`}>
                     <h2 className="pr-10 text-xl font-bold hover:underline">{game.name}</h2>
                 </Link>
                 <p className="pr-10 mt-2 text-gray-600"><b>Vývojáři:</b> {game.developers?.map((developer, index) => <Link to={`/developers/${developer.id}`} key={index} className="text-blue-500 hover:underline">{developer.name}{index !== game.developers.length - 1 && ', '}</Link>)}</p>
-                <p className="mt-2 text-gray-600"><b>Datum vydání:</b> {game.releaseDate}</p>
+                <p className="mt-2 text-gray-600"><b>Datum vydání:</b> {new Date(game.releaseDate).toLocaleDateString()}</p>
                 <p className="mt-2 text-gray-600"><b>Žánry:</b> {game.genres?.map((genre, index) => <Link to={`/games?genre=${genre.id}`} key={index} className="text-blue-500 hover:underline">{genre.name}{index !== game.genres.length - 1 && ', '}</Link>)}</p>
                 <p className="mt-4 text-gray-700">
                     {game.description}
