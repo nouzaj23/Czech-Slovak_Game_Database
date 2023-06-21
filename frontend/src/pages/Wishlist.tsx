de62cb4d141e96fb808a81491fd8c37237d66b23
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-21 22:04:42 +0200

    fix(wishlist): fixed

--- frontend/src/pages/Wishlist.tsx
+++ frontend/src/pages/Wishlist.tsx
@@ -10,13 +10,16 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 export const WishList = () => {
     const { auth } = useAuth();
 
-    const { data: wishListData } = useQuery<String[]>(['users'], () => UserApi.getWishlist(auth.userId));
-    const wishlistGameIds = wishListData ?? [];
-    
-    const { data: gamesData } = useQuery<Game[]>(['games'], GameApi.retrieveAllGames);
-    const games = gamesData ?? [];
+    const { data: wishListData } = useQuery<string[]>(['users'], () => UserApi.getWishlist(auth.userId));
+    const wishlistGameIds: string[] = wishListData ?? [];
 
-    const wishlistGames = games.filter(game => wishlistGameIds.includes(game.id));
+    // const { data: gamesData } = useQuery<Game[]>(['games'], GameApi.retrieveAllGames);
+    // const games = gamesData ?? [];
+
+    const wishlistGames = wishlistGameIds.map(id => (useQuery<Game>(['games'], () => GameApi.retrieveGame(id))).data ?? undefined);
+
+
+    // const wishlistGames = games.filter(game => wishlistGameIds.includes(game.id));
 
     const queryClient = useQueryClient();
 
@@ -40,20 +43,23 @@ export const WishList = () => {
             <div className="w-full md:w-3/4">
                 <h1 className="text-4xl m-6">WishList</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-6">
-                    {wishlistGames.map((game, index) => (
-                        <div key={index} className="relative rounded overflow-hidden shadow-lg p-6 bg-white">
-                            <AiFillCloseCircle className="absolute bottom-2 right-2 text-2xl cursor-pointer" onClick={() => removeGameFromWishlist(game)} />
-                            <img className="w-full h-auto object-cover" src={game.cover} alt={game.name} />
-                            <Link to={`/games/${game.id}`}>
-                                <div className="font-bold text-xl text-center mt-4">{game.name}</div>
-                            </Link>
-                            <div className="flex justify-center px-6 py-4 min-w-0">
-                                <div className="w-1/3 inline-block text-white rounded-full px-3 py-1 shadow-md mb-2 text-center" style={{ background: ratingBg(reviews.filter(rev => game.reviews.map(r => r.id).includes(rev.id)).reduce((ac, cur) => ac + cur.rating, 0)) }}>
-                                    <p className="font-bold text-lg">{(reviews.filter(review => game.reviews.map(r => r.id).includes(review.id)).reduce((accumulator, currentValue) => accumulator + currentValue.rating, 0) / game.reviews.length).toFixed(1)}</p>
+                    {wishlistGames.map((game, index) => {
+                        if (game)
+                            return (
+                                <div key={index} className="relative rounded overflow-hidden shadow-lg p-6 bg-white">
+                                    <AiFillCloseCircle className="absolute bottom-2 right-2 text-2xl cursor-pointer" onClick={() => removeGameFromWishlist(game)} />
+                                    <img className="w-full h-auto object-cover" src={game.cover} alt={game.name} />
+                                    <Link to={`/games/${game.id}`}>
+                                        <div className="font-bold text-xl text-center mt-4">{game.name}</div>
+                                    </Link>
+                                    <div className="flex justify-center px-6 py-4 min-w-0">
+                                        <div className="w-1/3 inline-block text-white rounded-full px-3 py-1 shadow-md mb-2 text-center" style={{ background: ratingBg(reviews.filter(rev => game.reviews.map(r => r.id).includes(rev.id)).reduce((ac, cur) => ac + cur.rating, 0)) }}>
+                                            <p className="font-bold text-lg">{(reviews.filter(review => game.reviews.map(r => r.id).includes(review.id)).reduce((accumulator, currentValue) => accumulator + currentValue.rating, 0) / game.reviews.length).toFixed(1)}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    ))}
+                            )
+                    })};
                 </div>
             </div>
         </div>
