8cb6bc4ec1200ef573582b9a52520f39bbdcde5b
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-22 11:38:23 +0200

    fix(comments): 5 comments per page

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -15,7 +15,8 @@ export const GamePage = () => {
     const [selectedTab, setSelectedTab] = useState('reviews');
     const [isOpen, setIsOpen] = useState(false);
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
-    const [page, setPage] = useState(0);
+    const [pageReviews, setPageReviews] = useState(0);
+    const [pageComments, setPageComments] = useState(0);
     const [stars, setStars] = useState(3);
 
     if (!id) {
@@ -24,13 +25,13 @@ export const GamePage = () => {
 
     const { data: gameData } = useQuery<Game>(['games', id], () => GameApi.retrieveGame(id), { enabled: !!id });
     const game = gameData;
-    
-    const {data: gameReviewsData} = useQuery<Review[]>(['gameReviews', id], () => GameApi.retrieveGameReviews(id));
+
+    const { data: gameReviewsData } = useQuery<Review[]>(['gameReviews', id], () => GameApi.retrieveGameReviews(id));
     const gameReviews: Review[] = gameReviewsData ?? [];
 
     console.log(gameReviews.length)
 
-    const {data: gameCommentsData} = useQuery<Comment[]>(['gameComments', id], () => GameApi.retrieveGameComments(id));
+    const { data: gameCommentsData } = useQuery<Comment[]>(['gameComments', id], () => GameApi.retrieveGameComments(id));
     const gameComments: Comment[] = gameCommentsData ?? [];
 
     if (!game) {
@@ -92,16 +93,30 @@ export const GamePage = () => {
     }
 
     const itemsPerPage = 5;
-    const handleNextPage = () => {
-        setPage(page + 1);
+
+    const handleNextPageReviews = () => {
+        setPageReviews(pageReviews + 1);
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
-    const handlePreviousPage = () => {
-        if (page > 0) {
-            setPage(page - 1);
+    const handlePreviousPageReviews = () => {
+        if (pageReviews > 0) {
+            setPageReviews(pageReviews - 1);
             window.scrollTo({ top: 0, behavior: 'smooth' });
         }
     };
+
+    const handleNextPageComments = () => {
+        setPageComments(pageComments + 1);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+    const handlePreviousPageComments = () => {
+        if (pageComments > 0) {
+            setPageComments(pageComments - 1);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    };
+
+
 
     const renderContent = () => {
         switch (selectedTab) {
@@ -111,12 +126,12 @@ export const GamePage = () => {
                         <button onClick={handleAddReview} style={{ background: ratingBg() }} className="px-3 py-2 mb-5 mt-5 text-white rounded">Přidat recenzi</button>
                         <div>
                             {isOpen && (
-                                <AddReviewForm gameId={game.id} game={game} setStars={setStars} stars={stars}/>
+                                <AddReviewForm gameId={game.id} game={game} setStars={setStars} stars={stars} />
                             )}
                         </div>
                         <div>
                             <div className="review-list space-y-4">
-                                {gameReviews.slice(page * itemsPerPage, (page + 1) * itemsPerPage).map((review, key) => (
+                                {gameReviews.slice(pageReviews * itemsPerPage, (pageReviews + 1) * itemsPerPage).map((review, key) => (
                                     <ReviewItem
                                         review={review}
                                         key={key}
@@ -126,18 +141,18 @@ export const GamePage = () => {
                             </div>
                             <div className="flex justify-between mt-4">
                                 <button
-                                    onClick={handlePreviousPage}
-                                    style={{ background: ratingBg()}}
-                                    className={`px-4 py-2 text-white rounded ${page === 0 && 'opacity-50 cursor-not-allowed'}`}
-                                    disabled={page === 0}
+                                    onClick={handlePreviousPageReviews}
+                                    style={{ background: ratingBg() }}
+                                    className={`px-4 py-2 text-white rounded ${pageReviews === 0 && 'opacity-50 cursor-not-allowed'}`}
+                                    disabled={pageReviews === 0}
                                 >
                                     Předchozí
                                 </button>
                                 <button
-                                    onClick={handleNextPage}
-                                    style={{ background: ratingBg()}}
-                                    className={`px-4 py-2 text-white rounded ${gameReviews.length <= (page + 1) * itemsPerPage && 'opacity-50 cursor-not-allowed'}`}
-                                    disabled={gameReviews.length <= (page + 1) * itemsPerPage}
+                                    onClick={handleNextPageReviews}
+                                    style={{ background: ratingBg() }}
+                                    className={`px-4 py-2 text-white rounded ${gameReviews.length <= (pageReviews + 1) * itemsPerPage && 'opacity-50 cursor-not-allowed'}`}
+                                    disabled={gameReviews.length <= (pageReviews + 1) * itemsPerPage}
                                 >
                                     Další
                                 </button>
@@ -155,8 +170,28 @@ export const GamePage = () => {
                             )}
                         </div>
                         <div className="review-list space-y-4">
-                            {gameComments.map((comment, index) => <CommentItem key={index} comment={comment}/>)}
+                            {gameComments.slice(pageComments * itemsPerPage, (pageComments + 1) * itemsPerPage).map((comment, index) =>
+                                <CommentItem key={index} comment={comment} />)}
                         </div>
+                        <div className="flex justify-between mt-4">
+                            <button
+                                onClick={handlePreviousPageComments}
+                                style={{ background: ratingBg() }}
+                                className={`px-4 py-2 text-white rounded ${pageComments === 0 && 'opacity-50 cursor-not-allowed'}`}
+                                disabled={pageComments === 0}
+                            >
+                                Předchozí
+                            </button>
+                            <button
+                                onClick={handleNextPageComments}
+                                style={{ background: ratingBg() }}
+                                className={`px-4 py-2 text-white rounded ${gameComments.length <= (pageComments + 1) * itemsPerPage && 'opacity-50 cursor-not-allowed'}`}
+                                disabled={gameComments.length <= (pageComments + 1) * itemsPerPage}
+                            >
+                                Další
+                            </button>
+                        </div>
+
                     </div>
                 );
             case 'photos':
@@ -197,7 +232,7 @@ export const GamePage = () => {
     return (
         <div className="w-full md:w-3/4 mx-auto p-4 bg-white shadow rounded">
             <div className="bg-white shadow-lg rounded-lg">
-                <GameCard game={game}/>
+                <GameCard game={game} />
             </div>
             <div className="mt-4 flex justify-around gap-1 sm:gap-4">
                 <button onClick={() => setSelectedTab('reviews')} style={{ background: ratingBg() }} className="px-3 py-2 text-sm sm:text-lg text-white w-full hover:bg-blue-600 transition-all duration-300 ease-in-out shadow-md hover:shadow-lg">
