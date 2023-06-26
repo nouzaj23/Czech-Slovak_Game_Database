fa164034e6fc0850415a66fb837aa618c792609e
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-26 12:30:38 +0200

    fix(gamepage): fixed images

--- frontend/src/components/Photos.tsx
+++ frontend/src/components/Photos.tsx
@@ -44,37 +44,6 @@ const ImagesGrid: React.FC<GridProps> = ({numColumns, game, setSelectedImage}) =
 
 
 export const Photos: React.FC<PhotosProps> = ({ game, setSelectedImage }) => {
-    // const handleImageClick = (image: string) => {
-    //     setSelectedImage(image);
-    // };
-
-    // const ImagesGrid = (numColumns: number) => {
-    //     const numRows: number = Math.ceil(game.photos.length / numColumns);
-
-    //     const gridItems = [];
-
-    //     for (let row: number = 0; row < numRows; row++) {
-    //         for (let col = 0; col < numColumns; col++) {
-    //             const index = row * numColumns + col;
-    //             if (index < game.photos.length) {
-    //                 const image = game.photos[index];
-    //                 gridItems.push(
-    //                     <div className="col-span-1 p-2 border-4 flex items-center justify-center" key={index}>
-    //                         <div
-    //                             className={`aspect-w-1 aspect-h-1 cursor-pointer`}
-    //                             onClick={() => handleImageClick(image)}
-    //                         >
-    //                             <img src={image} alt={`Obrázek ${index + 1}`} className="object-cover max-w-full max-h-full" />
-    //                         </div>
-    //                     </div>
-
-    //                 );
-    //             }
-    //         }
-    //     }
-    //     return gridItems;
-    // }
-
     return (
         <div className="container">
             <div className="hidden sm:block">
