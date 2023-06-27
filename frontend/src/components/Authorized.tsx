932c366fc160275c7784ce71244ece95934a69fe
Jan Nouza <xnouza@fi.muni.cz>
2023-06-27 16:49:06 +0200

    fix(Authorized): Fixed auth check

--- frontend/src/components/Authorized.tsx
+++ frontend/src/components/Authorized.tsx
@@ -41,9 +41,11 @@ type WishListProps = {
 export const AddToFavourite: FC<WishListProps> = ({ children, id }) => {
     const { auth } = useAuth();
     const { data: user } = useQuery(['user', auth?.userId], () => UserApi.retrieve(auth?.userId), {
-        enabled: !!auth,
+        enabled: !!auth?.userId,
     });
-    const { data: wishListData } = useQuery<string[]>(['users'], () => UserApi.getWishlist(auth.userId));
+    const { data: wishListData } = useQuery<string[]>(['users'], () => UserApi.getWishlist(auth?.userId), {
+        enabled: !!auth?.userId,
+    });
     const wishlistGameIds: string[] = wishListData ?? [];
 
     if (!auth || !user) return null;
@@ -54,9 +56,11 @@ export const AddToFavourite: FC<WishListProps> = ({ children, id }) => {
 export const RemoveFromFavourite: FC<WishListProps> = ({ children, id }) => {
     const { auth } = useAuth();
     const { data: user } = useQuery(['user', auth?.userId], () => UserApi.retrieve(auth?.userId), {
-        enabled: !!auth,
+        enabled: !!auth?.userId,
     });
-    const { data: wishListData } = useQuery<string[]>(['users'], () => UserApi.getWishlist(auth.userId));
+    const { data: wishListData } = useQuery<string[]>(['users'], () => UserApi.getWishlist(auth?.userId), {
+        enabled: !!auth?.userId,
+    });
     const wishlistGameIds: string[] = wishListData ?? [];
 
     if (!auth || !user) return null;
