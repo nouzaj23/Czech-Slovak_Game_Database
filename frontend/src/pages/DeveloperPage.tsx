c76a17471e744fde6ce84c498ec887d8c01868ae
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-28 20:26:54 +0200

    fix(register): errors fixed

--- frontend/src/pages/DeveloperPage.tsx
+++ frontend/src/pages/DeveloperPage.tsx
@@ -2,10 +2,12 @@ import { Link, useParams } from "react-router-dom";
 import { Developer, Game } from "../models";
 import { DeveloperApi, GameApi } from "../services";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 
 export const DeveloperPage = () => {
     const { id } = useParams<{ id: string }>();
+    const [page, setPage] = useState(0);
 
     if (!id) {
         return <div>404</div>;
@@ -25,6 +27,19 @@ export const DeveloperPage = () => {
         return <div>Vývojář není k dispozici</div>;
     }
 
+    const itemsPerPage = 1;
+
+    const handleNextPage = () => {
+        setPage(page + 1);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+    const handlePreviousPage = () => {
+        if (page > 0) {
+            setPage(page - 1);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    };
+
     return (
         <div className="w-full md:w-3/4 mx-auto p-4 bg-white shadow rounded">
             <div className="bg-white shadow-lg rounded-lg p-6 relative" style={{ overflow: 'auto' }}>
@@ -43,6 +58,22 @@ export const DeveloperPage = () => {
                         </div>
                     </Link>
                 ))}
+            </div>
+            <div className="flex justify-between mt-4 pb-4">
+                <button
+                    onClick={handlePreviousPage}
+                    className={`px-4 py-2 ml-5 text-white bg-gray-600 hover:bg-gray-800 rounded rounded-md ${page === 0 && 'opacity-50 cursor-not-allowed'}`}
+                    disabled={page === 0}
+                >
+                    Předchozí
+                </button>
+                <button
+                    onClick={handleNextPage}
+                    className={`px-4 py-2 mr-5 text-white bg-gray-600 hover:bg-gray-800 rounded rounded-md ${devGames.length <= (page + 1) * itemsPerPage && 'opacity-50 cursor-not-allowed'}`}
+                    disabled={devGames.length <= (page + 1) * itemsPerPage}
+                >
+                    Další
+                </button>
             </div>
         </div>
     )
