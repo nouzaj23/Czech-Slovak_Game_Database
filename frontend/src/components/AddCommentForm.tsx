70d9b3a1307a36091eb09686e9e70a3f979a6cbb
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-28 13:55:51 +0200

    fix(comments): fix submit

--- frontend/src/components/AddCommentForm.tsx
+++ frontend/src/components/AddCommentForm.tsx
@@ -46,7 +46,8 @@ export const AddCommentForm: React.FC<AddCommentProps> = ({ game }) => {
             <div>
                 <button type="submit"
                     value="Přidat komentář"
-                    className="p-2 bg-blue-500 text-white border-none cursor-pointer rounded" />
+                    className="p-2 bg-blue-500 text-white border-none cursor-pointer rounded">Přidat komentář
+                </button>
             </div>
         </div>
     );
