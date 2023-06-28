ddf1968fb423d82dc17a83b4678b358c58e4c81a
Tomas Rabel <xrabel@fi.muni.cz>
2023-06-28 20:14:17 +0200

    fix(register): errors fixed

--- frontend/src/pages/DeveloperPage.tsx
+++ frontend/src/pages/DeveloperPage.tsx
@@ -2,7 +2,6 @@ import { Link, useParams } from "react-router-dom";
 import { Developer, Game } from "../models";
 import { DeveloperApi, GameApi } from "../services";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
 
 
 export const DeveloperPage = () => {
@@ -26,20 +25,6 @@ export const DeveloperPage = () => {
         return <div>Vývojář není k dispozici</div>;
     }
 
-    const [page, setPage] = useState(0);
-    const itemsPerPage = 1;
-
-    const handleNextPage = () => {
-        setPage(page + 1);
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-    const handlePreviousPage = () => {
-        if (page > 0) {
-            setPage(page - 1);
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
-    };
-
     return (
         <div className="w-full md:w-3/4 mx-auto p-4 bg-white shadow rounded">
             <div className="bg-white shadow-lg rounded-lg p-6 relative" style={{ overflow: 'auto' }}>
@@ -50,7 +35,7 @@ export const DeveloperPage = () => {
                 </div>
             </div>
             <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 p-4 mt-4">
-                {devGames.slice(page * itemsPerPage, (page + 1) * itemsPerPage).map((game) => (
+                {devGames.map((game) => (
                     <Link to={`/games/${game.id}`} key={game.id}>
                         <div className="flex flex-col hover:underline">
                             <img src={game.cover} alt="Game cover" className="w-full my-auto" />
@@ -58,22 +43,6 @@ export const DeveloperPage = () => {
                         </div>
                     </Link>
                 ))}
-                <div className="flex justify-between mt-4 pb-4">
-                    <button
-                        onClick={handlePreviousPage}
-                        className={`px-4 py-2 ml-5 text-white bg-gray-600 hover:bg-gray-800 rounded rounded-md ${page === 0 && 'opacity-50 cursor-not-allowed'}`}
-                        disabled={page === 0}
-                    >
-                        Předchozí
-                    </button>
-                    <button
-                        onClick={handleNextPage}
-                        className={`px-4 py-2 mr-5 text-white bg-gray-600 hover:bg-gray-800 rounded rounded-md ${devGames.length <= (page + 1) * itemsPerPage && 'opacity-50 cursor-not-allowed'}`}
-                        disabled={devGames.length <= (page + 1) * itemsPerPage}
-                    >
-                        Další
-                    </button>
-                </div>
             </div>
         </div>
     )
